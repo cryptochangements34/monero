a408bc4aa0cc7140ef42126a18a5d3c389c19a9d
cryptochangements34 <bW9uZXJv@gmail.com>
2019-07-03 23:31:14 -0400

    ribbon green: get trades during latest 20 blocks

--- src/cryptonote_core/ribbon.cpp
+++ src/cryptonote_core/ribbon.cpp
@@ -3,9 +3,12 @@
 #include <iostream>
 
 #include "rapidjson/document.h"
+#include "blockchain.h"
 #include "ribbon.h"
 
 namespace service_nodes {
+
+cryptonote::Blockchain* m_blockchain_storage;
 
 size_t curl_write_callback(char *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -13,10 +16,8 @@ size_t curl_write_callback(char *contents, size_t size, size_t nmemb, void *user
     return size * nmemb;
 }
 
-// TODO: use http_client from net tools
-std::vector<trade_oger_trade> get_trades_from_ogre()
+std::string make_curl_http_get(std::string url)
 {
-  std::string url = "https://tradeogre.com/api/v1/history/BTC-XTRI";
   std::string read_buffer;
   CURL* curl = curl_easy_init(); 
   
@@ -25,17 +26,24 @@ std::vector<trade_oger_trade> get_trades_from_ogre()
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
   curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
   CURLcode res = curl_easy_perform(curl); 
-  curl_easy_cleanup(curl); 
-  if (res != CURLE_OK)
-    std::cout << "not ok" << std::endl;
+  curl_easy_cleanup(curl);
+  
+  return read_buffer;
+}
+
+
+// TODO: use http_client from net tools
+std::vector<trade_ogre_trade> get_trades_from_ogre()
+{
+  std::string data = make_curl_http_get(std::string(TRADE_OGRE_API) + std::string("/history/BTC-XTRI"));
     
   rapidjson::Document document;
-  document.Parse(read_buffer.c_str());
+  document.Parse(data.c_str());
   
-  std::vector<trade_oger_trade> trades;
+  std::vector<trade_ogre_trade> trades;
   for (size_t i = 0; i < document.Size(); i++)
   {
-    trade_oger_trade trade;
+    trade_ogre_trade trade;
     trade.date = document[i]["date"].GetInt();
     trade.type = document[i]["type"].GetString();
     trade.type = document[i]["price"].GetString();
@@ -44,6 +52,30 @@ std::vector<trade_oger_trade> get_trades_from_ogre()
   }
   
   return trades;
+}
+
+std::vector<trade_ogre_trade> trades_during_latest_20_blocks(std::vector<trade_ogre_trade> trades)
+{
+  uint64_t top_block_height = m_blockchain_storage->get_current_blockchain_height() - 1;
+  crypto::hash top_block_hash = m_blockchain_storage->get_block_id_by_height(top_block_height);
+  cryptonote::block top_blk;
+  m_blockchain_storage->get_block_by_hash(top_block_hash, top_blk);
+  uint64_t top_block_timestamp = top_blk.timestamp;
+  
+  uint64_t twenty_block_height = m_blockchain_storage->get_current_blockchain_height() - 21;
+  crypto::hash twenty_block_hash = m_blockchain_storage->get_block_id_by_height(twenty_block_height);
+  cryptonote::block twenty_blk;
+  m_blockchain_storage->get_block_by_hash(twenty_block_hash, twenty_blk);
+  uint64_t twenty_block_timestamp = twenty_blk.timestamp;
+  
+  std::vector<trade_ogre_trade> result;
+  for (size_t i = 0; i < trades.size(); i++)
+  {
+    if (trades[i].date <= top_block_timestamp && trades[i].date >= twenty_block_timestamp)
+      result.push_back(trades[i]);
+  }
+  
+  return result;
 }
 
 }
--- src/cryptonote_core/ribbon.h
+++ src/cryptonote_core/ribbon.h
@@ -1,12 +1,15 @@
 namespace service_nodes {
 
-struct trade_oger_trade {
-  uint32_t date;
+#define TRADE_OGRE_API "https://tradeogre.com/api/v1"
+
+struct trade_ogre_trade {
+  uint64_t date;
   std::string type;
   std::string price;
   std::string quantity;
 };
 
-std::vector<trade_oger_trade> get_trades_from_ogre();
+std::vector<trade_ogre_trade> get_trades_from_ogre();
+std::vector<trade_ogre_trade> trades_during_latest_20_blocks(std::vector<trade_ogre_trade> trades);
 
 }
